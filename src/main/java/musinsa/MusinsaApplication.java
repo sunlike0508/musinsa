eee5ee187e9c26f020c728edf2bbc7342f50ba29
seonhoshin <sunlike0508@waug.com>
2024-07-09 20:54:21 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/musinsa/MusinsaApplication.java
@@ -0,0 +1,13 @@
+package musinsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusinsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/musinsa/MusinsaApplicationTests.java
@@ -0,0 +1,13 @@
+package musinsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
